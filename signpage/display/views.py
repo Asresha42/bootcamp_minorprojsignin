b4eb1325478d0029e453b1f2aeab625db9f64f30
Asha Karanam <asresha42@gmail.com>
2020-07-06 00:56:34 -0500

    final update

--- signpage/display/views.py
+++ signpage/display/views.py
@@ -4,7 +4,8 @@ import json
 
 
 # Create your views here.
-
+def signup(request):
+    return render(request, 'signup.html')
 
 def signin(request):
     return render(request, 'signin.html')
@@ -25,5 +26,23 @@ def submitUser(request):
     }
 
     response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.text
+    return render(request, 'temp.html', {'data': data})
+
+def sUser(request):
+
+    email = request.GET['Emailid']
+    password = request.GET['Password']
+    name = request.GET['Username']
+    print(email, password, name, "this is me")
+
+    url = "http://127.0.0.1:8000/api/login/"
+
+    payload = {'email': email, 'password': password, 'name': name}
+    payload = json.dumps(payload)
+    headers = {
+         'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
     data = response.text
     return render(request, 'temp.html', {'data': data})
--- signpage/minorproj/settings.py
+++ signpage/minorproj/settings.py
@@ -124,3 +124,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
+
